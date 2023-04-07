d70a59adae52f2cbf2ea9b98188ccb14de02c7c7
Josue Gallegos <gallegosjosue6@gmail.com>
2023-04-07 13:33:02 -0500

    complete first class func lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (){
+   const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+   return drivers.splice(0,2);
+}
+
+const returnLastTwoDrivers = function (){
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return drivers.splice(-2);
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers, 
+    returnLastTwoDrivers
+];
+
+function createFareMultiplier(num){
+    return function(number){
+        return number * num;
+    }
+}
+
+function fareDoubler(num){
+    return num * 2;
+}
+
+function fareTripler(num){
+    return num * 3;
+}
+
+function selectDifferentDrivers(drivers, func){
+    return func(drivers);
+}
